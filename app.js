7c92db01aa0d1df9e4556dcd0596e0c08648efb5
Chris Lindgren <chris.a.lindgren@gmail.com>
2019-08-27 16:53:22 -0400

    new config

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {StaticMap} from 'react-map-gl';
 import DeckGL, {IconLayer} from 'deck.gl';
-
 import IconClusterLayer from './icon-cluster-layer';
 
 // Set your mapbox token here
