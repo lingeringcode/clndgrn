641e65c5158338bb30b79a9ca7c055772aa24e6b
Chris Lindgren <chris.a.lindgren@gmail.com>
2019-08-27 20:08:53 -0400

    vars

--- apikey.js
+++ apikey.js
@@ -1,6 +1,4 @@
 import resolve from "path"
 import config from "dotenv"
-config({ path: resolve(__dirname, "./.env") })
 
-// const fs = require('fs')
-// fs.writeFileSync('./.env', `MAPBOX_API_KEY=${process.env.MAPBOX_API_KEY}\n`)
+config({ path: resolve(__dirname, "./.env") });
