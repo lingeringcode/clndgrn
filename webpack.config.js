ab1f303091e90a270e4f124711581228555d02c5
Chris Lindgren <chris.a.lindgren@gmail.com>
2020-11-04 14:58:46 -0500

    updated app

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
   },
   plugins: [
     new Dotenv({
+      path: './.env', // load this now instead of the ones in '.env'
       systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
       silent: false // hide any errors
     })
